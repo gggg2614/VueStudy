380f19451b3e9648951868e7fc380ffaadc9bca2
gggg2614 <839857519@qq.com>
2022-06-30 18:22:56 +0800

    submit

---
+++ VueLoader/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home :luo="luos" :num="nums"></Home>
+  </div>
+</template>
+
+<script>
+import Home from "./src/views/home.vue";
+export default {
+  components: {
+    Home,
+  },
+  data() {
+    return {
+      luos: {
+      },
+      nums: [123,120],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
